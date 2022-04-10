51d47c091e9333394d09ece2248d9ee58856b01e
Pallav Jain <jain.pallav1998gmail.com>
2022-04-10 20:08:06 +0530

    Navbar added

---
+++ Components/LandingPage/LandingPage.jsx
@@ -0,0 +1,87 @@
+import React from "react";
+import styles from "./Homepage.module.css";
+// import video from "./hero.mp4";
+import { Subjectimg, twiter } from "./Localdata";
+import Link from "next/link";
+
+function LandingPage() {
+  return (
+    <div className={styles.mainbox}>
+      {/* <div
+        style={{
+          position: "relative",
+          zIndex: "-1",
+          width: "100%",
+          height: "90%",
+        }}
+      >
+        <video width="100%" autoPlay muted loop>
+          <source src={video} type="video/mp4" />
+        </video>
+      </div> */}
+      <div>
+        {/* <div className={styles.Onvideo}>
+          <h1 className={styles.contHeading}>
+            <strong className={styles.heading}>Advance Your Skills</strong>
+            <br />
+            <span>with In-Depth, Modern</span>
+            <br />
+            <span>Front-End Engineering</span>
+            <br />
+            <div>
+              <Link href="/">
+                <button className={styles.button}>Join Now</button>
+              </Link>
+            </div>
+          </h1>
+        </div> */}
+      </div>
+      <div>
+        <svg
+          style={{
+            height: "70px",
+            width: "100%",
+            zIndex: "5",
+            marginBottom: "6px",
+          }}
+        >
+          <polygon
+            points="0,60 0,30 1700,60"
+            style={{ fill: "#c76767", stroke: "none", strokeWidth: "1" }}
+          />
+        </svg>
+      </div>
+
+      <div className={styles.Subjectflex}>
+        <div className={styles.Subject}>
+          {Subjectimg.map((el) => (
+            <div key={el.id}>
+              <img id={styles.Subimage} src={el.image} alt="logo" />
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className={styles.MainImageBox}>
+        <h1>
+          Join thousands of professionals who already use XYZ to ace their
+          interviews
+        </h1>
+
+        <div className={styles.ImageBox}>
+          {twiter.map((item) => (
+            <div key={item.id} className={styles.Combody}>
+              <div className={styles.Imagebody}>
+                <img src={item.img} alt="" />
+                <h3 className={styles.Chead}>{item.name}</h3>
+              </div>
+              <div className={styles.Chead}>{item.des}</div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default LandingPage;
---
+++ Components/NavBar/Navbar.jsx
@@ -0,0 +1,71 @@
+import Link from "next/link";
+import styles from "./Navbar.module.css";
+import { cardBgColor } from "../Colors/colors";
+
+function Navbar({ Address, ConnectWallet, DisconnectWallet }) {
+  return (
+    <div className={styles.Navbody}>
+      <div className={styles.Navbars}>
+        <Link href="/">
+          <img
+            height="40px"
+            src="https://images.vexels.com/media/users/3/216751/raw/a525182aedd0f213326b3682eabfa5ee-online-study-logo-template.jpg"
+            alt="LOGO"
+          />
+        </Link>
+
+        <div
+          style={{
+            width: "400px",
+            justifyContent: "space-between",
+            display: "flex",
+            minWidth: "400px",
+            paddingTop: "10px",
+          }}
+        >
+          <div className={styles.hover}>
+            <Link href="/" className={styles.links}>
+              Dashboard
+            </Link>
+          </div>
+          <div className={styles.hover}>
+            <Link href="/" className={styles.links}>
+              Learn
+            </Link>
+          </div>
+          <div className={styles.hover}>
+            <Link href="/" className={styles.links}>
+              Guides
+            </Link>
+          </div>
+          <div className={styles.hover}>
+            {!Address ? (
+              <button className={styles.buttonlink} onClick={ConnectWallet}>
+                Login
+              </button>
+            ) : (
+              <button onClick={DisconnectWallet} className={styles.buttonlink}>
+                Logout
+              </button>
+            )}
+          </div>
+        </div>
+      </div>
+      <svg
+        style={{
+          height: "70px",
+          width: "100%",
+          zIndex: "1",
+          marginTop: "-2px",
+        }}
+      >
+        <polygon
+          points="-300,0 1400,-30 1700,20"
+          style={{ fill: { cardBgColor }, stroke: "none", strokeWidth: "1" }}
+        />
+      </svg>
+    </div>
+  );
+}
+
+export default Navbar;
